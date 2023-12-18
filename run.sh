5d917b55104e2c36993f63a3d6fa26d8faaf9ff9
6ministers <manyatovskiy@gmail.com>
2023-12-18 23:54:29 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,6 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 
-# clone Flectra directory
-git clone --depth=1 https://github.com/6Ministers/flectra-2.0dev-docker-compose $DESTINATION
-rm -rf $DESTINATION/.git
-
-# set permission
-mkdir -p $DESTINATION/postgresql
-sudo chmod -R 777 $DESTINATION
-
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
