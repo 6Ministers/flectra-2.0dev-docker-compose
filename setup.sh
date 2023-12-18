c8590f8fbe94b905561ad12b66a71aa66859d0ed
6ministers <manyatovskiy@gmail.com>
2023-12-19 00:31:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# This script setups dockerized Redash on Ubuntu 20.04.
-
+# This script setups dockerized Redash on Ubuntu 22.04.
 set -eu
 
 install_docker() {
   # Install Docker
   export DEBIAN_FRONTEND=noninteractive
+  sudo apt-get purge needrestart
   sudo apt-get -qqy update
   DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
   sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen gnupg
