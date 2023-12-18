b20b1c6d4be22d1b6db314d56bd22f0536ab61c1
6ministers <manyatovskiy@gmail.com>
2023-12-18 22:39:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+DESTINATION=$1
+PORT=$2
+CHAT=$3
+
+# clone Flectra directory
+git clone --depth=1 https://github.com/minhng92/flectra-2.0-docker-compose $DESTINATION
+rm -rf $DESTINATION/.git
+
+# set permission
+mkdir -p $DESTINATION/postgresql
+sudo chmod -R 777 $DESTINATION
+
+# config
+if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
+sudo sysctl -p
+sed -i 's/10020/'$PORT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/20020/'$CHAT'/g' $DESTINATION/docker-compose.yml
+
 # This script setups dockerized Redash on Ubuntu 20.04.
 set -eu
 
@@ -24,23 +42,8 @@ install_docker() {
   # Allow current user to run Docker commands
   sudo usermod -aG docker "$USER"
 }
-
 install_docker
 
-DESTINATION=$1
-PORT=$2
-CHAT=$3
-# clone Flectra directory
-git clone --depth=1 https://github.com/minhng92/flectra-2.0-docker-compose $DESTINATION
-rm -rf $DESTINATION/.git
-# set permission
-mkdir -p $DESTINATION/postgresql
-sudo chmod -R 777 $DESTINATION
-# config
-if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
-sudo sysctl -p
-sed -i 's/10020/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20020/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Flectra
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
